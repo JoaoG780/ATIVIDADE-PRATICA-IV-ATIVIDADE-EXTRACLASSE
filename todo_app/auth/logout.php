4210b9fb4ba833054097d7169c34ffd62f68cbdf
JoaoG780 <joaobielsilva780@gmail.com>
2025-11-27 14:12:04 -0300

    Delete todo_app directory

--- todo_app/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-session_unset();
-session_destroy();
-header('Location: /todo_app/auth/login.php');
-exit;
--- todo_app/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/todo_app/assets/js/main.js"></script>
-</body>
-</html>
